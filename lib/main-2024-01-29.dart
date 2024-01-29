39d9adf04c2d1a9ae904187a5714738962c10921
wx <xing.wang@xforwardai.com>
2024-01-29 11:14:32 +0800

    苹果和安卓 后台消息提示

---
+++ lib/main-2024-01-29.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+// 每个文件导出一个MyApp
+// import './01常用/01.文本组件.dart';
+// import '01常用/02.容器组件Container.dart';
+
+// import '01常用/03.线性布局Column-Row.dart';
+// import '01常用/04.弹性布局.dart';
+// import '01常用/05.流式布局-Wrap.dart';
+// import '01常用/06.层叠布局.dart';
+// import './01常用/07.Card.dart';
+// import './01常用/08.按钮.dart';
+// import './01常用/09.列表组件-SingleChildScrollView.dart';
+// import './01常用/09.列表组件-ListView.dart';
+// import './01常用/09.列表组件-GridView.dart';
+// import './01常用/10.平台风格-弹窗.dart';
+// import './02常用的三方组件/11.dio.dart';
+// import './02常用的三方组件/12.flutter_swiper.dart';
+// import './02常用的三方组件/13.shared_preferences.dart';
+// import './01常用/14.数据共享-InheritedWidget.dart';
+// import './02常用的三方组件/16.数据共享-Provider.dart';
+// import './03路由相关/17.匿名路由.dart';
+// import './03路由相关/18.命名路由.dart';
+// import './03路由相关/19.动态路由.dart';
+// import './03路由相关/20.底部导航.dart';
+// import './03路由相关/21.tabs导航.dart';
+
+// import './04表单/22.Switch.dart';
+// import './04表单/23.Checkbox.dart';
+// import './04表单/24.Radio.dart';
+// import './04表单/25.TextField.dart';
+// import './04表单/26.日历组件.dart';
+// import './04表单/27.Form.dart';
+// import './05其他/28.动画-01.dart';
+// import '05其他/28.动画-02多个动画交织.dart';
+
+// import './05其他/28.动画-03-Hero.dart';
+
+// import './05其他/29.国际化-01-内置组件.dart';
+// import '05其他/29.国际化-02-自定义组件的国际化.dart';
+// import './05其他/29.国际化-03实际开发.dart';
+// import './05其他/30.多主题.dart';
+// import './06数据状态-flutter_redux/31.flutter_redux.dart';
+// import '06数据状态-flutter_redux复杂例子/32.index.dart';
+// import './06数据状态mob/33.mobx.dart';
+// import './06mobx-demo/34.count_page.dart';
+// import './07.GetX/01.Dialog.dart';
+// import '07.GetX/02.路由相关-onGenerateRoute.dart';
+// import './07.GetX/03.路由相关-GetPage.dart';
+// import '07.GetX/04.状态管理-01-响应式数据.dart';
+// import './07.GetX/04.状态管理-02-依赖管理.dart';
+// import './07.GetX/04.状态管理-03-依赖管理-优化02.dart';
+import './07.GetX/05.demo.dart';
+
+void main() => runApp(MyApp());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
